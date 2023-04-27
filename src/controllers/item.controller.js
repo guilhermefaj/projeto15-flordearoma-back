02ff25974fd06072be0942f0f8717eaa5e52e5c5
guilhermefaj <figueiredoguiu@gmail.com>
2023-04-27 19:12:23 -0300

    feat: Adição de controller e rota de item

---
+++ src/controllers/item.controller.js
@@ -0,0 +1,14 @@
+import { db } from "../database/database.connection";
+
+export async function getItems(req, res) {
+    const { itemId } = req.params
+    const { id, name, value, category, URL, stock } = req.body
+
+    try {
+        const items = await db.collection("products").find({ id: itemId }).toArray()
+
+        res.status(200).send(items)
+    } catch {
+        res.status(500).send(err.message)
+    }
+}
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import usersRouter from "./users.routes.js";
+import itemRouter from "./item.routes.js";
 
 const router = Router();
 router.use(usersRouter);
+router.use(itemRouter);
 
 export default router;
