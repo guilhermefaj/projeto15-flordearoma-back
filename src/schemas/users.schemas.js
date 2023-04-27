d4326d8e5f9fb7af876187ac958389e207673e99
larissa <larissaalves1102@gmail.com>
2023-04-27 01:21:41 -0300

    fix: error message validation password

--- src/schemas/users.schemas.js
+++ src/schemas/users.schemas.js
@@ -4,14 +4,14 @@ export const userSchema = joi.object({
     firstName: joi.string().required(),
     lastName: joi.string().required(),
     email: joi.string().email().required(),
-    password: joi.string().regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/).error(errors => {
-        return new Error('A senha deve conter pelo menos uma letra e um número');
+    password: joi.string().min(5).regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/).error(errors => {
+        return new Error('A senha deve ter no mínimo 5 caracteres e conter pelo menos uma letra e um número.');
     }).required()
 })
 
 export const userLoginSchema = joi.object({
     email: joi.string().email().required(),
-    password: joi.string().regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/).error(errors => {
-        return new Error('A senha deve conter pelo menos uma letra e um número');
+    password: joi.string().min(5).regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/).error(errors => {
+        return new Error('A senha deve ter no mínimo 5 caracteres e conter pelo menos uma letra e um número.');
     }).required()
 })
